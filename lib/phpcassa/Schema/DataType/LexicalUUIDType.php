1a90d1932762f94546383d98a5e88b65b7a341c1
Tyler Hobbs <tyler@datastax.com>
2012-03-17 01:40:19 -0500

    Auto serialize and unserialize UUID types

--- lib/phpcassa/Schema/DataType/LexicalUUIDType.php
+++ lib/phpcassa/Schema/DataType/LexicalUUIDType.php
@@ -1,7 +1,9 @@
 <?php
 namespace phpcassa\Schema\DataType;
 
+use phpcassa\Schema\DataType\UUIDType;
+
 /**
  * @package phpcassa\Schema\DataType
  */
-class LexicalUUIDType extends CassandraType { }
+class LexicalUUIDType extends UUIDType {}
--- lib/phpcassa/Schema/DataType/UUIDType.php
+++ lib/phpcassa/Schema/DataType/UUIDType.php
@@ -4,4 +4,20 @@ namespace phpcassa\Schema\DataType;
 /**
  * @package phpcassa\Schema\DataType
  */
-class UUIDType extends CassandraType { }
+class UUIDType extends CassandraType
+{
+    public function pack($value, $is_name=true, $slice_end=null, $is_data=false) {
+        if ($is_name && $is_data)
+            $value = unserialize($value);
+        return $value->bytes;
+    }
+
+    public function unpack($data, $is_name=true) {
+        $value = UUID::import($data);
+        if ($is_name) {
+            return serialize($value);
+        } else {
+            return $value;
+        }
+    }
+}
