3f83eda2fa0244f4245badd4091a18d596d48756
Tyler Hobbs <tyler@datastax.com>
2012-03-24 13:23:27 -0500

    Update autopack values test

--- test/Autopacking/AutopackValuesTest.php
+++ test/Autopacking/AutopackValuesTest.php
@@ -6,15 +6,6 @@ use phpcassa\Schema\DataType;
 use phpcassa\SystemManager;
 
 use phpcassa\UUID;
-use phpcassa\UUID\UUIDGen;
-
-use phpcassa\UUID\DataType\LongType;
-use phpcassa\UUID\DataType\IntegerType;
-use phpcassa\UUID\DataType\BytesType;
-use phpcassa\UUID\DataType\AsciiType;
-use phpcassa\UUID\DataType\UTF8Type;
-use phpcassa\UUID\DataType\LexicalUUIDType;
-use phpcassa\UUID\DataType\TimeUUIDType;
 
 class AutopackValuesTest extends PHPUnit_Framework_TestCase {
 
@@ -43,20 +34,8 @@ class AutopackValuesTest extends PHPUnit_Framework_TestCase {
         $cfattrs["default_validation_class"] = DataType::LONG_TYPE;
         $sys->create_column_family(self::$KS, 'ValidatorLong', $cfattrs);
 
-        $cfattrs["default_validation_class"] = DataType::INTEGER_TYPE;
-        $sys->create_column_family(self::$KS, 'ValidatorInt', $cfattrs);
-
         $cfattrs["default_validation_class"] = DataType::TIME_UUID_TYPE;
         $sys->create_column_family(self::$KS, 'ValidatorTime', $cfattrs);
-
-        $cfattrs["default_validation_class"] = DataType::LEXICAL_UUID_TYPE;
-        $sys->create_column_family(self::$KS, 'ValidatorLex', $cfattrs);
-
-        $cfattrs["default_validation_class"] = DataType::ASCII_TYPE;
-        $sys->create_column_family(self::$KS, 'ValidatorAscii', $cfattrs);
-
-        $cfattrs["default_validation_class"] = DataType::UTF8_TYPE;
-        $sys->create_column_family(self::$KS, 'ValidatorUTF8', $cfattrs);
 
         $cfattrs["default_validation_class"] = DataType::BYTES_TYPE;
         $sys->create_column_family(self::$KS, 'ValidatorBytes', $cfattrs);
@@ -80,12 +59,6 @@ class AutopackValuesTest extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
         $this->client = new ConnectionPool(self::$KS);
-
-        $this->TIME2 = UUIDGen::uuid1();
-        $this->TIME3 = UUIDGen::uuid1();
-
-        $this->LEX2 = UUID::import('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')->bytes;
-        $this->LEX3 = UUID::import('cccccccccccccccccccccccccccccccc')->bytes;
     }
 
     public function tearDown() { }
@@ -97,41 +70,12 @@ class AutopackValuesTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($col, $this->cf_valid_long->get(self::$KEYS[0]));
     }
 
-    public function test_integers() {
-        $this->cf_valid_int = new ColumnFamily($this->client, 'ValidatorInt');
-        $col = array('subcol' => 2);
-        $this->cf_valid_int->insert(self::$KEYS[0], $col);
-        $this->assertEquals($col, $this->cf_valid_int->get(self::$KEYS[0]));
-    }
-
     public function test_time_uuids() {
-        $this->TIME = UUIDGen::uuid1();
+        $time = UUID::mint();
         $this->cf_valid_time = new ColumnFamily($this->client, 'ValidatorTime');
-        $col = array('subcol' => $this->TIME);
+        $col = array('subcol' => $time);
         $this->cf_valid_time->insert(self::$KEYS[0], $col);
         $this->assertEquals($col, $this->cf_valid_time->get(self::$KEYS[0]));
-    }
-
-    public function test_lexical_uuids() {
-        $this->LEX = UUID::import('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')->bytes;
-        $this->cf_valid_lex = new ColumnFamily($this->client, 'ValidatorLex');
-        $col = array('subcol' => $this->LEX);
-        $this->cf_valid_lex->insert(self::$KEYS[0], $col);
-        $this->assertEquals($col, $this->cf_valid_lex->get(self::$KEYS[0]));
-    }
-
-    public function test_ascii() {
-        $this->cf_valid_ascii = new ColumnFamily($this->client, 'ValidatorAscii');
-        $col = array('subcol' => 'aaa');
-        $this->cf_valid_ascii->insert(self::$KEYS[0], $col);
-        $this->assertEquals($col, $this->cf_valid_ascii->get(self::$KEYS[0]));
-    }
-
-    public function test_utf8() {
-        $this->cf_valid_utf8 = new ColumnFamily($this->client, 'ValidatorUTF8');
-        $col = array('subcol' => "a&#1047;");
-        $this->cf_valid_utf8->insert(self::$KEYS[0], $col);
-        $this->assertEquals($col, $this->cf_valid_utf8->get(self::$KEYS[0]));
     }
 
     public function test_bytes() {
@@ -150,17 +94,16 @@ class AutopackValuesTest extends PHPUnit_Framework_TestCase {
     }
 
     public function test_default_validated_columns() {
-        $this->TIME = UUIDGen::uuid1();
-        $this->LEX = UUID::import('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')->bytes;
+        $time = UUID::mint();
         $this->cf_def_valid = new ColumnFamily($this->client, 'DefaultValidator');
         $col_cf = array('aaaaaa' => 222222222222);
-        $col_cm = array('subcol' => $this->TIME);
+        $col_cm = array('subcol' => $time);
 
         # Both of these inserts work, as cf allows
         # longs and cm for 'subcol' allows TimeUUIDs
         $this->cf_def_valid->insert(self::$KEYS[0], $col_cf);
         $this->cf_def_valid->insert(self::$KEYS[0], $col_cm);
         $this->assertEquals($this->cf_def_valid->get(self::$KEYS[0]),
-                          array('aaaaaa' => 222222222222, 'subcol' => $this->TIME));
+                          array('aaaaaa' => 222222222222, 'subcol' => $time));
     }
 }
