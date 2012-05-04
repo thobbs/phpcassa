109910dc7e1670500278dd5f5de091bd8d07e7da
Tyler Hobbs <tyler@datastax.com>
2012-05-04 13:34:40 -0500

    Additional key_validation_class tests

--- test/Autopacking/AutopackKeysTest.php
+++ test/Autopacking/AutopackKeysTest.php
@@ -3,6 +3,8 @@ require_once(__DIR__.'/AutopackBase.php');
 
 use phpcassa\Connection\ConnectionPool;
 use phpcassa\ColumnFamily;
+use phpcassa\Index\IndexClause;
+use phpcassa\Index\IndexExpression;
 use phpcassa\Schema\DataType;
 use phpcassa\SystemManager;
 
@@ -17,8 +19,7 @@ class AutopackKeysTest extends AutopackBase {
 
         $cfattrs["key_validation_class"] = DataType::LONG_TYPE;
         $sys->create_column_family(self::$KS, 'LongKeys', $cfattrs);
-        $sys->create_index(self::$KS, 'LongKeys', 'subcol',
-            DataType::TIME_UUID_TYPE, NULL, NULL);
+        $sys->create_index(self::$KS, 'LongKeys', 'subcol', DataType::LONG_TYPE);
     }
 
     public function setUp() {
@@ -26,7 +27,9 @@ class AutopackKeysTest extends AutopackBase {
         $this->cf = new ColumnFamily($this->client, 'LongKeys');
     }
 
-    public function tearDown() { }
+    public function tearDown() {
+        $this->client->close();
+    }
 
     public function test_get() {
         $this->cf->insert(123, array("foo" => "bar"));
@@ -51,14 +54,40 @@ class AutopackKeysTest extends AutopackBase {
     }
 
     public function test_get_range() {
-        $this->cf = new ColumnFamily($this->client, 'LongKeys');
         $this->cf->truncate();
-        $this->cf->insert(1, array("a" => "a"));
-        $this->cf->insert(2, array("b" => "b"));
-        $res_array = array();
+        $this->cf->insert(0, array("a" => "a"));
+        $this->cf->insert(1, array("b" => "b"));
+        $this->cf->insert(2, array("c" => "c"));
+
+        $this->cf->buffer_size = 2;
+
         $res = iterator_to_array($this->cf->get_range());
-        $this->assertEquals(array("1" => array("a" => "a"),
-                                  "2" => array("b" => "b")),
+        $this->assertEquals(array("0" => array("a" => "a"),
+                                  "1" => array("b" => "b"),
+                                  "2" => array("c" => "c")),
                             $res);
+    }
+
+    public function test_get_indexed_slices() {
+        $this->cf->truncate();
+        $this->cf->insert(0, array("subcol" => 0));
+        $this->cf->insert(1, array("subcol" => 1));
+        $this->cf->insert(2, array("subcol" => 1));
+        $this->cf->insert(3, array("subcol" => 1));
+
+        $this->cf->buffer_size = 2;
+
+        $expr = new IndexExpression("subcol", 1);
+        $clause = new IndexClause(array($expr));
+        $res = iterator_to_array($this->cf->get_indexed_slices($clause));
+        $this->assertEquals(array(1 => array("subcol" => 1),
+                                  2 => array("subcol" => 1),
+                                  3 => array("subcol" => 1)),
+                            $res);
+
+        $expr = new IndexExpression("subcol", 0);
+        $clause = new IndexClause(array($expr));
+        $res = iterator_to_array($this->cf->get_indexed_slices($clause));
+        $this->assertEquals(array(0 => array("subcol" => 0)), $res);
     }
 }
