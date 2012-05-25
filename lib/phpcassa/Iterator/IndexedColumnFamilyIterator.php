89749c55a40a5758498d3d4cedb78efe345cd50a
Tyler Hobbs <tyler@datastax.com>
2012-05-25 16:54:09 -0500

    Don't fetch an extra row when getting low numbers of rows

--- lib/phpcassa/Iterator/IndexedColumnFamilyIterator.php
+++ lib/phpcassa/Iterator/IndexedColumnFamilyIterator.php
@@ -9,13 +9,15 @@ namespace phpcassa\Iterator;
  */
 class IndexedColumnFamilyIterator extends ColumnFamilyIterator {
 
-    private $index_clause;
+    private $index_clause, $buffer_number;
 
     public function __construct($column_family, $index_clause, $buffer_size,
                                 $column_parent, $predicate,
                                 $read_consistency_level) {
 
         $this->index_clause = $index_clause;
+        $this->buffer_number = 0;
+
         $row_count = $index_clause->count;
         $orig_start_key = $index_clause->start_key;
 
@@ -26,11 +28,18 @@ class IndexedColumnFamilyIterator extends ColumnFamilyIterator {
 
     protected function get_buffer() {
         # Figure out how many rows we need to get and record that
-        if($this->row_count != null)
-            $this->index_clause->count = min($this->row_count - $this->rows_seen + 1, $this->buffer_size);
-        else
-            $this->index_clause->count = $this->buffer_size;
-        $this->expected_page_size = $this->index_clause->count;
+        $buff_sz = $this->buffer_size;
+        if($this->row_count !== null) {
+            if ($this->buffer_number == 0 && $this->row_count <= $buff_sz) {
+                // we don't need to chunk, grab exactly the right number of rows
+                $buff_sz = $this->row_count;
+            } else {
+                $buff_sz = min($this->row_count - $this->rows_seen + 1, $this->buffer_size);
+            }
+        }
+        $this->expected_page_size = $buff_sz;
+        $this->index_clause->count = $buff_sz;
+        $this->buffer_number++;
 
         $this->index_clause->start_key = $this->column_family->pack_key($this->next_start_key);
         $resp = $this->column_family->pool->call("get_indexed_slices",
--- lib/phpcassa/Iterator/RangeColumnFamilyIterator.php
+++ lib/phpcassa/Iterator/RangeColumnFamilyIterator.php
@@ -11,7 +11,7 @@ use cassandra\KeyRange;
  */
 class RangeColumnFamilyIterator extends ColumnFamilyIterator {
 
-    private $key_start, $key_finish;
+    private $key_start, $key_finish, $buffer_number;
 
     public function __construct($column_family, $buffer_size,
                                 $key_start, $key_finish, $row_count,
@@ -20,6 +20,7 @@ class RangeColumnFamilyIterator extends ColumnFamilyIterator {
 
         $this->key_start = $key_start;
         $this->key_finish = $key_finish;
+        $this->buffer_number = 0;
 
         parent::__construct($column_family, $buffer_size, $row_count,
                             $key_start, $column_parent, $predicate,
@@ -27,11 +28,17 @@ class RangeColumnFamilyIterator extends ColumnFamilyIterator {
     }
 
     protected function get_buffer() {
-        if($this->row_count != null)
-            $buff_sz = min($this->row_count - $this->rows_seen + 1, $this->buffer_size);
-        else
-            $buff_sz = $this->buffer_size;
+        $buff_sz = $this->buffer_size;
+        if($this->row_count !== null) {
+            if ($this->buffer_number == 0 && $this->row_count <= $buff_sz) {
+                // we don't need to chunk, grab exactly the right number of rows
+                $buff_sz = $this->row_count;
+            } else {
+                $buff_sz = min($this->row_count - $this->rows_seen + 1, $this->buffer_size);
+            }
+        }
         $this->expected_page_size = $buff_sz;
+        $this->buffer_number++;
 
         $key_range = new KeyRange();
         $key_range->start_key = $this->column_family->pack_key($this->next_start_key);
