39bf1105927f9116e1d2f92e268c791dc3d6d741
Tyler Hobbs <tyler@datastax.com>
2012-05-25 18:04:47 -0500

    Unit tests to reproduce #77

--- test/Autopacking/AutopackKeysTest.php
+++ test/Autopacking/AutopackKeysTest.php
@@ -15,16 +15,27 @@ class AutopackKeysTest extends AutopackBase {
     public static function setUpBeforeClass() {
         parent::setUpBeforeClass();
         $sys = new SystemManager();
-        $cfattrs = array("column_type" => "Standard");
 
-        $cfattrs["key_validation_class"] = DataType::LONG_TYPE;
+        $cfattrs = array(
+            "column_type" => "Standard",
+            "key_validation_class" => DataType::LONG_TYPE
+        );
         $sys->create_column_family(self::$KS, 'LongKeys', $cfattrs);
         $sys->create_index(self::$KS, 'LongKeys', 'subcol', DataType::LONG_TYPE);
+
+        $cfattrs = array(
+            "column_type" => "Standard",
+            "key_validation_class" => DataType::UUID_TYPE
+        );
+        $sys->create_column_family(self::$KS, 'UUIDKeys', $cfattrs);
+        $sys->create_index(self::$KS, 'UUIDKeys', 'subcol', DataType::LONG_TYPE);
+        $sys->create_index(self::$KS, 'UUIDKeys', 'subcol', DataType::LONG_TYPE);
     }
 
     public function setUp() {
         $this->client = new ConnectionPool(self::$KS);
         $this->cf = new ColumnFamily($this->client, 'LongKeys');
+        $this->uuid_cf = new ColumnFamily($this->client, 'UUIDKeys');
     }
 
     public function tearDown() {
@@ -36,12 +47,29 @@ class AutopackKeysTest extends AutopackBase {
         $this->assertEquals(array("foo" => "bar"), $this->cf->get(123));
     }
 
+    public function test_get_serialized() {
+        $uuid = UUID::uuid1();
+        $this->uuid_cf->insert($uuid, array("foo" => "bar"));
+        $this->assertEquals(array("foo" => "bar"), $this->uuid_cf->get($uuid));
+    }
+
     public function test_multiget() {
         $this->cf->insert(1, array("a" => "a"));
         $this->cf->insert(2, array("b" => "b"));
         $res = $this->cf->multiget(array(1, 2));
         $this->assertEquals(array(1 => array("a" => "a"),
                                   2 => array("b" => "b")),
+                            $res);
+    }
+
+    public function test_multiget_serialized() {
+        $uuid1 = UUID::uuid1();
+        $uuid2 = UUID::uuid1();
+        $this->uuid_cf->insert($uuid1, array("a" => "a"));
+        $this->uuid_cf->insert($uuid2, array("b" => "b"));
+        $res = $this->uuid_cf->multiget(array($uuid1, $uuid2));
+        $this->assertEquals(array(serialize($uuid1) => array("a" => "a"),
+                                  serialize($uuid2) => array("b" => "b")),
                             $res);
     }
 
@@ -53,6 +81,15 @@ class AutopackKeysTest extends AutopackBase {
         $this->cf->get(123);
     }
 
+    public function test_remove_serialized() {
+        $uuid = UUID::uuid1();
+        $this->uuid_cf->insert($uuid, array("foo" => "bar"));
+        $this->assertEquals(array("foo" => "bar"), $this->uuid_cf->get($uuid));
+        $this->uuid_cf->remove($uuid);
+        $this->setExpectedException('\cassandra\NotFoundException');
+        $this->uuid_cf->get($uuid);
+    }
+
     public function test_get_range() {
         $this->cf->truncate();
         $this->cf->insert(0, array("a" => "a"));
@@ -62,9 +99,26 @@ class AutopackKeysTest extends AutopackBase {
         $this->cf->buffer_size = 2;
 
         $res = iterator_to_array($this->cf->get_range());
-        $this->assertEquals(array("0" => array("a" => "a"),
-                                  "1" => array("b" => "b"),
-                                  "2" => array("c" => "c")),
+        $this->assertEquals(array(0 => array("a" => "a"),
+                                  1 => array("b" => "b"),
+                                  2 => array("c" => "c")),
+                            $res);
+    }
+
+    public function test_get_range_serialized() {
+        $this->uuid_cf->truncate();
+        $uuid1 = UUID::uuid1();
+        $uuid2 = UUID::uuid1();
+        $uuid3 = UUID::uuid1();
+        $this->uuid_cf->insert($uuid1, array("a" => "a"));
+        $this->uuid_cf->insert($uuid2, array("b" => "b"));
+        $this->uuid_cf->insert($uuid3, array("c" => "c"));
+
+        $this->uuid_cf->buffer_size = 2;
+        $res = iterator_to_array($this->uuid_cf->get_range());
+        $this->assertEquals(array(serialize($uuid1) => array("a" => "a"),
+                                  serialize($uuid2) => array("b" => "b"),
+                                  serialize($uuid3) => array("c" => "c")),
                             $res);
     }
 
@@ -90,4 +144,30 @@ class AutopackKeysTest extends AutopackBase {
         $res = iterator_to_array($this->cf->get_indexed_slices($clause));
         $this->assertEquals(array(0 => array("subcol" => 0)), $res);
     }
+
+    public function test_get_indexed_slices_serialized() {
+        $this->uuid_cf->truncate();
+
+        $uuid1 = UUID::uuid1();
+        $uuid2 = UUID::uuid1();
+        $uuid3 = UUID::uuid1();
+        $this->uuid_cf->insert($uuid1, array("subcol" => 0));
+        $this->uuid_cf->insert($uuid2, array("subcol" => 1));
+        $this->uuid_cf->insert($uuid3, array("subcol" => 1));
+
+        $this->uuid_cf->buffer_size = 2;
+
+        $expr = new IndexExpression("subcol", 1);
+        $clause = new IndexClause(array($expr));
+        $res = iterator_to_array($this->uuid_cf->get_indexed_slices($clause));
+        $this->assertEquals(array(serialize($uuid2) => array("subcol" => 1),
+                                  serialize($uuid3) => array("subcol" => 1)),
+                            $res);
+
+        $expr = new IndexExpression("subcol", 0);
+        $clause = new IndexClause(array($expr));
+        $res = iterator_to_array($this->uuid_cf->get_indexed_slices($clause));
+        $this->assertEquals(array(serialize($uuid1) => array("subcol" => 0)), $res);
+    }
+
 }
