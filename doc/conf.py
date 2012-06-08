3b4e32010d71a443d97c4a4a39f30a0e3bc27bbd
Tyler Hobbs <tyler@datastax.com>
2012-06-08 15:34:34 -0500

    Bump version to 1.0.a.3, update changelog, AUTHORS

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,7 @@ project = 'phpcassa'
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0.a.2'
+release = '1.0.a.3'
 
 # List of documents that shouldn't be included in the build.
 unused_docs = []
